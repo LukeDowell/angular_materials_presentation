86e45ace66a814b18c86c45c46ef38c40df5d9fc
Luke Dowell <lukedowell@gmail.com>
2015-08-14 10:24:06 -0500

    Initial Commit

---
+++ client/scripts/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by lukedowell on 8/14/15.
+ */
+var myApp = angular.module('myApp', []);
---
+++ server/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by lukedowell on 8/14/15.
+ */
+var express = require('express');
