15c1831657f82f76b37c632cc4b2cf641b189243
Luke Dowell <lukedowell@gmail.com>
2015-08-14 14:12:38 -0500

    Finishes our demonstration

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,17 +3,29 @@
  */
 var app = angular.module('myApp', ['ngMaterial']);
 
-app.controller('MyController', ['$scope', '$mdSidenav', function($scope, $mdSidenav) {
-    $scope.user = {
-        firstName: "Maria",
-        company: "Prime"
-    };
+app.controller('MyController', ['$scope', '$mdSidenav', '$mdDialog',
+    function($scope, $mdSidenav, $mdDialog) {
+        var alert;
 
-    $scope.toggleSidenav = function(menuId) {
-        $mdSidenav(menuId).toggle();
-    };
+        $scope.user = {
+            firstName: "Maria"
+        };
 
-    $scope.sendAlert = function() {
-        alert("Hell0!");
-    }
+        $scope.toggleSidenav = function(menuId) {
+            $mdSidenav(menuId).toggle();
+        };
+
+        $scope.sendAlert = function() {
+            alert = $mdDialog.alert({
+                title: "Example Title",
+                content: "This is an example of how easy it is to create dialog boxes",
+                ok: "Close"
+            });
+            $mdDialog
+                .show(alert)
+                .finally(function() {
+                    console.log("Dialog close");
+                    alert = undefined;
+                });
+        }
 }]);
