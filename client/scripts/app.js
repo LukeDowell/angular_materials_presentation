e5148f1a5fae8a176fd61a3e9d6f49e4dcbb7ce7
Luke Dowell <lukedowell@gmail.com>
2015-08-14 11:55:06 -0500

    Basic angular material functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,4 +1,19 @@
 /**
  * Created by lukedowell on 8/14/15.
  */
-var myApp = angular.module('myApp', ['ngMaterial']);
+var app = angular.module('myApp', ['ngMaterial']);
+
+app.controller('MyController', ['$scope', '$mdSidenav', function($scope, $mdSidenav) {
+    $scope.user = {
+        firstName: "Maria",
+        company: "Prime"
+    };
+
+    $scope.toggleSidenav = function(menuId) {
+        $mdSidenav(menuId).toggle();
+    };
+
+    $scope.sendAlert = function() {
+        alert("Hell0!");
+    }
+}]);
